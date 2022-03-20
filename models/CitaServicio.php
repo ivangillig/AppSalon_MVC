8edbc341447abbb201cf256578dadafbba25690a
ivangillig <ivan.gillig@gmail.com>
2022-03-20 13:04:44 -0300

    ...

--- models/CitaServicio.php
+++ models/CitaServicio.php
@@ -6,15 +6,15 @@ class CitaServicio extends ActiveRecord{
 
     protected static $tabla = 'citasServicios';
     //sirve para normalizar los datos
-    protected static $columnasDB = ['id', 'citaId', 'serviciosId'];
+    protected static $columnasDB = ['id', 'citaId', 'servicioId'];
     
     public $id;
-    public $citasId;
+    public $citaId;
     public $serviciosId;
 
     public function __construct($args = []) {
         $this->id = $args['id'] ?? null;
-        $this->citasId = $args['citaId'] ?? '';
-        $this->serviciosId = $args['serviciosId'] ?? '';
+        $this->citaId = $args['citaId'] ?? '';
+        $this->servicioId = $args['servicioId'] ?? '';
     }
 }
